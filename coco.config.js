1a0ca147c5b9458f54bf7a800169fb854afed7b0
muwoo <2424880409@qq.com>
2021-04-13 09:56:23 +0800

    ref: 远程组件

---
+++ coco.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  webDomian: 'https://coco-h5.github.io/coco-global-banner',
+  gitUrl: 'git@github.com:coco-h5/coco-global-banner.git',
+  name: 'coco-global-banner',
+  description: "远程banner组件"
+}
---
+++ packages/coco-global-banner2/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :href="obj.link">
+    <img
+      class="global-banner"
+      :src="obj.src"
+      alt="图片"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'banner',
+  props: {
+    obj: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+<style>
+.global-banner {
+  width: 100%;
+}
+</style>
